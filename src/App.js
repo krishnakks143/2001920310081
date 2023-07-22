030ca4698fa3b2fd49f1d5e966ce155f301dc94c
krishnakks143 <ec20081@glbitm.ac.in>
2023-07-22 20:20:56 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const NumberManagementApp = () => {
+  const [urls, setUrls] = useState('');
+  const [numbers, setNumbers] = useState([]);
+
+  const fetchNumbers = async () => {
+    const urlsArray = urls.trim().split(/\s+/);
+    const promises = urlsArray.map((url) => axios.get(url, { timeout: 500 }));
+    try {
+      const responses = await Promise.allSettled(promises);
+      let mergedNumbers = [];
+      responses.forEach((response) => {
+        if (response.status === 'fulfilled' && response.value.status === 200) {
+          mergedNumbers = [...mergedNumbers, ...response.value.data.numbers];
+        }
+      });
+      setNumbers([...new Set(mergedNumbers)].sort((a, b) => a - b));
+    } catch (error) {
+      console.error('Error fetching numbers:', error);
+    }
+  };
+
+  const handleFetch = () => {
+    fetchNumbers();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Number Management App</h1>
+      <div className="form">
+        <label htmlFor="urls">Enter URLs (separated by space):</label>
+        <input
+          type="text"
+          id="urls"
+          value={urls}
+          onChange={(e) => setUrls(e.target.value)}
+          placeholder="Enter URLs..."
+        />
+        <button onClick={handleFetch}>Fetch Numbers</button>
+      </div>
+      <div className="result">
+        <h2>Result:</h2>
+        <pre>{JSON.stringify(numbers, null, 2)}</pre>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default NumberManagementApp;
